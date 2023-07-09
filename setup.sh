418d0578ace8e988b9e8e3c9233693a60070ff06
neko_hat <dohwan90@gmail.com>
2023-07-09 14:38:47 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@ sudo apt-get update && sudo apt-get install patchelf -y
 
 CURRENT_PATH=`pwd`
 cd ~
-mkdir pwninit && cd pwninit
+mkdir .pwninit && cd .pwninit
 wget https://github.com/io12/pwninit/releases/download/3.3.0/pwninit
 chmod +x pwninit
 LINK_PATH=`pwd`
-sudo ln -s $LINK_PATH/pwninit /usr/local/bin
+sudo ln -s $LINK_PATH/.pwninit /usr/local/bin
 
 cd $CURRENT_PATH
