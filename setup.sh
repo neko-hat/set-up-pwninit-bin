19ebb8fe2e9155850b8f84981423c255bf36ba62
neko_hat <dohwan90@gmail.com>
2023-08-08 00:52:22 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,6 +8,6 @@ mkdir .pwninit && cd .pwninit
 wget https://github.com/io12/pwninit/releases/download/3.3.0/pwninit
 chmod +x pwninit
 LINK_PATH=`pwd`
-sudo ln -s $LINK_PATH/.pwninit /usr/local/bin
+sudo ln -s $LINK_PATH/pwninit /usr/local/bin
 
 cd $CURRENT_PATH
